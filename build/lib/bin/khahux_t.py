42f5cf60931a85631aee901417cb0e95ea0decd6
90h <195022586@qq.com>
2019-11-11 17:56:25 +0800

    print_function

--- build/lib/bin/khahux_t.py
+++ build/lib/bin/khahux_t.py
@@ -1,7 +1,8 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
 
 import json
-from distutils.log import warn as printf
 import sys
 try:
     from urllib import urlencode
@@ -16,11 +17,11 @@ KEY = 1132608537
 KEYFROM = 'khahux'
 
 CODE_MSG_MAP = {
-    20: '要翻译的文本过长',
-    30: '无法进行有效的翻译',
-    40: '不支持的语言类型',
-    50: '无效的key',
-    60: '无词典结果，仅在获取词典结果生效'
+    20: u'要翻译的文本过长',
+    30: u'无法进行有效的翻译',
+    40: u'不支持的语言类型',
+    50: u'无效的key',
+    60: u'无词典结果，仅在获取词典结果生效'
 }
 
 
@@ -43,13 +44,13 @@ def get_translate(q):
 def show_json(json_data):
     error_code = json_data.get('errorCode')
     if error_code != 0:
-        return printf("""
+        return print(u"""
     [{}]{}
         """.format(error_code, CODE_MSG_MAP.get(error_code)))
 
     query = json_data.get('query')
     translations = json_data.get('translation')
-    printf('\n[{}]: {}'.format(query, ', '.join(translations)))
+    print(u'\n[{}]: {}'.format(query, ', '.join(translations)))
 
     basic = json_data.get('basic')
     if basic and isinstance(basic, dict):
@@ -57,21 +58,21 @@ def show_json(json_data):
         uk = basic.get('uk-phonetic', None)
         us = basic.get('us-phonetic', None)
         explains = basic.get('explains', [])
-        printf('[phonetic] {}, [us] {}, [uk] {}'.format(phonetic, uk, us))
-        printf('\n')
-        printf(', '.join(explains))
+        print(u'[phonetic] {}, [us] {}, [uk] {}'.format(phonetic, uk, us))
+        print(u'\n')
+        print(u', '.join(explains))
 
     web = json_data.get('web')
     if web and isinstance(web, list):
         for item in web:
-            printf('{0}: {1}'.format(item['key'], ','.join(item['value'])))
+            print(u'{0}: {1}'.format(item['key'], ','.join(item['value'])))
 
-    printf('\n')
+    print(u'\n')
 
 
 def main():
     if not len(sys.argv) > 1:
-        return printf("""
+        return print(u"""
     Usage:
     khahux_t <word>
         """)
